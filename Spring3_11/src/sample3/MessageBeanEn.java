75000ec66fa2332ebeacfa945cc53be85b1bd03c
KangSeok_Mac <KangSeok_Mac@Kang_Mac-PC>
2018-09-02 00:31:57 +0900

    .

---
+++ Spring3_11/src/sample3/MessageBeanEn.java
@@ -0,0 +1,11 @@
+package sample3;
+
+public class MessageBeanEn implements MessageBean {
+
+	@Override
+	public void sayHello(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, " + name + "!");
+	}
+
+}
---
+++ Spring3_11/src/sample3/MessageBeanKr.java
@@ -0,0 +1,11 @@
+package sample3;
+
+public class MessageBeanKr implements MessageBean {
+
+	@Override
+	public void sayHello(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요, " + name + " 씨.");
+	}
+
+}
