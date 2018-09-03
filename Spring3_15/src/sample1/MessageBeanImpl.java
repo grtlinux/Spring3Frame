b987535420d493ba1679f1bd9a88e96b556ead74
KangSeok_Mac <KangSeok_Mac@Kang_Mac-PC>
2018-09-03 21:15:17 +0900

    .

---
+++ Spring3_15/src/sample1/MessageBeanImpl.java
@@ -0,0 +1,25 @@
+package sample1;
+
+public class MessageBeanImpl implements MessageBean {
+
+	private String name;
+	private String message;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		try { Thread.sleep(5000); } catch (InterruptedException e) {}
+
+		System.out.println("Hello, " + this.name + "!");
+		System.out.printf("message is [%s]%n", this.message);
+	}
+
+}
